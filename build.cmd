87121d55eae68a8902a5325f7f00d2bdeecf2486
leekelleher <leekelleher@gmail.com>
2015-12-01 12:41:42 +0000

    Added NuGet restore call.
    Updated NuGet.exe

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.proj
+CALL src\.nuget\NuGet.exe restore src\ZipDiff.sln
+CALL "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" build.proj
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 @exit /B 0
 :err
